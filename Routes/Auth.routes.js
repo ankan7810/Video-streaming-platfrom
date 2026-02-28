2b11d7da22caa3078ecbe9414cfcb1f90ad7a7fa
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-02-28 10:51:17 +0530

    Rename reset route and add change password route

--- Routes/Auth.routes.js
+++ Routes/Auth.routes.js
@@ -1,7 +1,6 @@
 import express from "express"
-import { loginUser, logoutUser, registerUser, resetPassword, sendOtp, verifyOtp } from "../Controllers/Auth.Controllers.js"
+import { changePassword, loginUser, logoutUser, registerUser, resetPassword, sendOtp, verifyOtp } from "../Controllers/Auth.Controllers.js"
 import { upload } from "../Middlewares/Multer.js"
-// import { registerUser, loginUser, getCurrentUser } from "../Controllers/Auth.controller"
 
 const authrouter = express.Router()
 
@@ -10,6 +9,7 @@ authrouter.post("/login", loginUser)
 authrouter.post("/logout", logoutUser)
 authrouter.post("/send-otp", sendOtp)
 authrouter.post("/verify-otp", verifyOtp)
-authrouter.post("/reset", resetPassword)
+authrouter.post("/reset-password", resetPassword)
+authrouter.post("/change-password", changePassword)
 
 export default authrouter
